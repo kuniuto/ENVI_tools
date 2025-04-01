0cea4ab6443ce5ca697e8b6f51ccf3f6420f7124
kuniuto <168343045+kuniuto@users.noreply.github.com>
2025-04-01 18:16:19 +0900

    Update envi_tool_demo_color_enhancement.py

--- envi_tool_demo_color_enhancement.py
+++ envi_tool_demo_color_enhancement.py
@@ -5,5 +5,5 @@ band_width = 40
 
 envi_fname = 'F:/bridge/data/ENVI/20240523/UV/ref_uv6_rice_Img-d(s20,g50,49.97ms,350-1100)_20240523_104142'
 
-envi_tool.envi2color_enhancement(envi_fname,color_wavelengths,band_width)
+envi_tool.color_enhancement(envi_fname,color_wavelengths,band_width)
 
